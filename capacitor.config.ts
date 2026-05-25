010b813c293badab898c14e49a908950c9e0c6fa
mkowalczyk111 <karol.czyz.97@gmail.com>
2026-05-25 13:12:10 +0200

    Routing i konfiguracja

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.fiszki.app',
-  appName: 'Fiszki',
+  appName: 'TensorDeck',
   webDir: 'dist'
 };
 
